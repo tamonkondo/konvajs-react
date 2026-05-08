a68fa33e23b4e3848febb0a213b3bdabd8568444
tamon kondo <tamon.kondo@japan-coding.co.jp>
2026-05-08 18:43:29 +0900

    フォルダ調整

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import path from 'node:path'
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [
